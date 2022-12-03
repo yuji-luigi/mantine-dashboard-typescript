6f4aae9c7ab9f061e6fe1f7baf027c7c2e72feca
Yuji Sato <--replace-all>
2022-12-03 23:47:21 +0100

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,35 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+/** @type {import('next').NextConfig} */
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: false,
+const nextConfig = {
+  // reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+  env: {
+    // api end point
+    // HOST_API_BASE_URL: 'http://flatmates-api.host:5000/api/v1',
+    HOST_API_BASE_URL: 'http://localhost:5000/api/v1',
+    HOST_API_BASE_URL_PRODUCTION: 'https://flatmates-api.yuji-luigi.com/api/v1',
+    API_VERSION: 'api/v1',
+     // MAPBOX
+     MAPBOX_API: '',
+     // FIREBASE
+     FIREBASE_API_KEY: '',
+     FIREBASE_AUTH_DOMAIN: '',
+     FIREBASE_PROJECT_ID: '',
+     FIREBASE_STORAGE_BUCKET: '',
+     FIREBASE_MESSAGING_SENDER_ID: '',
+     FIREBASE_APPID: '',
+     FIREBASE_MEASUREMENT_ID: '',
+     // AWS COGNITO
+     AWS_COGNITO_USER_POOL_ID: '',
+     AWS_COGNITO_CLIENT_ID: '',
+     // AUTH0
+     AUTH0_CLIENT_ID: '',
+     AUTH0_DOMAIN: '',
+  },
+}
+
+module.exports = nextConfig
