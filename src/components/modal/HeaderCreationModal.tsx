065b23a304a4d011f0232098abfef9f74e2ccd38
Yuji Sato <--replace-all>
2023-03-09 06:15:50 +0100

    creation modal for header

---
+++ src/components/modal/HeaderCreationModal.tsx
@@ -0,0 +1,82 @@
+import { useDisclosure } from '@mantine/hooks';
+
+import { Menu, Modal, Button, Group, Text, ActionIcon } from '@mantine/core';
+import {
+  IconSettings,
+  IconSearch,
+  IconPhoto,
+  IconMessageCircle,
+  IconTrash,
+  IconArrowsLeftRight,
+} from '@tabler/icons-react';
+import { Icons } from '../../data/icons';
+import { allSectionArrayWithRoles } from '../../data';
+
+import { FONT_SIZES } from '../../lib/enums';
+import ModalContent from './ModalContent';
+import { useState } from 'react';
+
+export function HeaderCreationModal() {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [section, setSection] = useState<SectionDataJsonWithRoles | null>(null);
+
+  const handleOpenModal = (modalType: ModalType) => {
+    console.log('l');
+    setSection(allSectionArrayWithRoles.find((section) => section.entity === modalType) || null);
+    open();
+  };
+  return (
+    <>
+      <Menu shadow="md" width={250}>
+        <Menu.Target>
+          <ActionIcon>
+            <Icons.plus />
+          </ActionIcon>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Label sx={{ textAlign: 'center' }}>Create something</Menu.Label>
+          <Menu.Item
+            onClick={() => handleOpenModal('posts')}
+            sx={{ fontSize: FONT_SIZES.menuItems }}
+            icon={<Icons.article size={FONT_SIZES.menuItems} />}
+          >
+            New post
+          </Menu.Item>
+          <Menu.Item
+            sx={{ fontSize: FONT_SIZES.menuItems }}
+            icon={<Icons.funds size={FONT_SIZES.menuItems} />}
+          >
+            New fund
+          </Menu.Item>
+          <Menu.Item
+            sx={{ fontSize: FONT_SIZES.menuItems }}
+            icon={<Icons.messageDots size={FONT_SIZES.menuItems} />}
+          >
+            Send message
+          </Menu.Item>
+          {/* <Menu.Item
+          icon={<IconSearch size={14} />}
+          rightSection={
+            <Text size="xs" color="dimmed">
+              ⌘K
+            </Text>
+          }
+        >
+          Search
+        </Menu.Item> */}
+
+          <Menu.Divider />
+
+          {/* <Menu.Label>Danger zone</Menu.Label>
+        <Menu.Item icon={<IconArrowsLeftRight size={14} />}>Transfer my data</Menu.Item>
+        <Menu.Item color="red" icon={<IconTrash size={14} />}>
+          Delete my account
+        </Menu.Item> */}
+        </Menu.Dropdown>
+      </Menu>
+      <Modal opened={opened} onClose={close} title={section?.createButton}>
+        <ModalContent />
+      </Modal>
+    </>
+  );
+}
---
+++ src/components/modal/ModalContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ModalContent = ({ modalType = null }: { modalType?: ModalType | null }) => {
+  return <div>ModalContent</div>;
+};
+
+export default ModalContent;
