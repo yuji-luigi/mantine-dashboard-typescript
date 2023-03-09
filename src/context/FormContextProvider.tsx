16cf09eb2575218619a402be7e432860f409df1c
Yuji Sato <--replace-all>
2023-03-09 09:23:34 +0100

    FormProvider combined Form Component

--- src/context/FormContextProvider.tsx
+++ src/context/FormContextProvider.tsx
@@ -1,5 +1,6 @@
 import { createFormContext } from '@mantine/form';
 import { TextInput } from '@mantine/core';
+import { ReactNode } from 'react';
 
 // Definition of form values is required
 interface FormValues {
@@ -11,28 +12,13 @@ interface FormValues {
 // FormProvider is a component that sets form context
 // useFormContext hook return form object that was previously set in FormProvider
 // useForm hook works the same way as useForm exported from the package but has predefined type
-const [FormProvider, useFormContext, useForm] = createFormContext<FormValues>();
 
-function ContextField() {
+function Form<FormDefaultValues>({ children }: { children: ReactNode }) {
+  const [FormProvider, useFormContext, useForm] = createFormContext<FormDefaultValues>();
   const form = useFormContext();
-  return <TextInput label="Your name" {...form.getInputProps('name')} />;
-}
-
-export function Context() {
-  // Create form as described in use-form documentation
-  const form = useForm({
-    initialValues: {
-      age: 0,
-      name: '',
-    },
-  });
-
-  // Wrap your form with FormProvider
   return (
     <FormProvider form={form}>
-      <form onSubmit={form.onSubmit(() => {})}>
-        <ContextField />
-      </form>
+      <form onSubmit={form.onSubmit(() => {})}>{children}</form>
     </FormProvider>
   );
 }
