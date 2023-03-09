d3965d2c7589122eceb007e96a4a35426976ecbd
Yuji Sato <--replace-all>
2023-03-09 06:16:48 +0100

    sectionData AllSectionWithRoles available. navBar by roles

--- src/data/index.ts
+++ src/data/index.ts
@@ -10,5 +10,23 @@ export const sections: string[] = en.reduce((arr, cur) => arr.concat(cur), []);
 
 export const flattenSectionData = beta.flatMap((sectionData) => sectionData.contents);
 
+export const superAdminEntities = beta
+  .filter((sectionData) => sectionData.roles.includes('super_admin'))
+  .flatMap((sectionData) => sectionData.contents.map((content) => content.entity));
+export const adminEntities = beta
+  .filter((sectionData) => sectionData.roles.includes('admin'))
+  .flatMap((sectionData) => sectionData.contents.map((content) => content.entity));
+
+// set programatically the roles for every sections
+export const allSectionArrayWithRoles = beta.flatMap((sectionData) => {
+  return sectionData.contents.map((content) => {
+    const modifiedContent = {
+      ...content,
+      roles: sectionData.roles,
+    };
+    return modifiedContent;
+  });
+});
+
 export const entities = flattenSectionData.map((sectionData) => sectionData.entity);
 export const sectionData = beta;
