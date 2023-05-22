78fbfab82015adaa0bd3e4a530478aa2d82109de
Yuji Sato <--replace-all>
2023-05-22 06:49:51 +0200

    file images added

--- src/lib/image-paths.ts
+++ src/lib/image-paths.ts
@@ -2,6 +2,17 @@ export const PATH_IMAGE = {
   rootSpaceCard1: '/images/haussmann-buildings3.png',
   flatmateLogo1: '/images/flatmate_circle_logo_yellow.png',
   faviconsvg: '/favicon.svg',
+  FILES: {
+    pdf: '/file_images/pdf.png',
+    pdf1: '/file_images/pdf_1.png',
+    pdf2: '/file_images/pdf_2.png',
+    doc: '/file_images/doc.png',
+    image: '/file_images/image.png',
+    jpg: '/file_images/jpg.png',
+    png: '/file_images/png.png',
+    txt: '/file_images/txt-file.png',
+    xls: '/file_images/xls.png',
+  },
 };
 
 export const IMAGES_ARRAY = Object.values(PATH_IMAGE);
